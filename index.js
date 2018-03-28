128e43164707884606c27e471aa880b93517b992
Robert LeCheminant <lecheminant@gmail.com>
2018-03-28 16:08:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much.";
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten (name){
-  kittens.push('Ralph');
+  kittens.push("Ralph");
   return (kittens);
 }
 
